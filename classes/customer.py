188973fe0c579e56d33419c3bfcbf8e4635edc60
jack-himlo <devon.g.jackson417@gmail.com>
2025-02-16 14:13:22 -0500

    Updated WIP, passing test 1-4, working on customer dict

--- classes/customer.py
+++ classes/customer.py
@@ -1,37 +1,59 @@
 # Write you Customer Class here
 class Customer:
     customer = {}
-    def __init__(self, customer_id, first_name, last_name, account_type):
-        self.customer_id = customer_id
+    counter = 0
+    def __init__(self, id, first_name, last_name, account_type, current_video_rentals = None):
+        self._id = id
+        Customer.counter += 1
         self.first_name = first_name
         self.last_name = last_name
-        self.account_type = account_type
-        self._current_rentals = []
-
+        self._account_type = account_type
+        self._current_video_rentals = current_video_rentals
+#set getters
     @property
     def id(self):
-        return self.customer_id
+        return self._id
     @property
-    def current_rentals(self):
-        return self._current_rentals
-    @current_rentals.setter
-    def current_rentals(self, rentals):
+    def current_video_rentals(self):
+        return self._current_video_rentals
+#rentals should return a list type, set setter
+    @current_video_rentals.setter
+    def current_video_rentals(self, rentals):
         if isinstance(rentals, list):
-            self._current_rentals = rentals
+            self._current_video_rentals = rentals
         elif isinstance(rentals, str):
-            self._current_rentals = [rentals]
+            self._current_video_rentals = [rentals]
         else:
             print(f"no current rentals available")
-    
-    def viewCustomer(self):
-        return {
-            "ID" : self.customer_id,
-            "First name" : self.first_name,
-            "Last name" : self.last_name,
-            "Account type": self.account_type,
-            "Current Videos Rented" : self._current_rentals
-        }
-    def addCustomer(self, customer_id, first_name, last_name, account_type):
-        id = 0 
-        customer_id = id + 1
-        fName = first_name
+#view each instance of a "customer"    
+    @classmethod
+    def viewCustomer(cls):
+        if not cls.customer:
+            print("No customers found.")
+            return
+        for _id, customer in cls.customer.items():
+            print(f"ID: {customer._id}")
+            print(f"First name:  {customer.first_name}")
+            print(f"Last name : {customer.last_name}")
+            print(f"Account type: {customer.account_type}")
+            print(f"Current Videos Rented: {customer._current_video_rentals}\n")
+        
+#add new customer class method
+    @classmethod
+    def addCustomer(cls):
+        #assign customer id
+        _id = cls.counter
+        #increasecounter for unique ID
+        cls.counter += 1
+        #gather customer info
+        first_name = input("Enter customer first name: ")
+        last_name = input("Enter customer last name: ")
+        account_type = input("Enter customer account type: ")
+        #add new customer to class level dict customers
+        newCustomer = cls(customer_id,first_name, last_name, account_type)
+        cls.customer[customer_id] = newCustomer
+        
+        print(f"Customer {first_name} {last_name}'s account has been added")
+
+
+
