d7739d243b1857cdac9fe137da0cff9755dbf7d1
jack-himlo <devon.g.jackson417@gmail.com>
2025-02-17 12:26:54 -0500

    Updated WIP, passing test 10

--- classes/customer_types.py
+++ classes/customer_types.py
@@ -1,15 +1,27 @@
 # Write your independent Customer account type classes here
-class Customer_sx:
-    pass
+#import Customer from classes.customer.py
+from classes.customer import Customer
+
+#inherint Customer 
+class Customer_sx(Customer):
+    def __init__(self, id, first_name, last_name, account_type = "sx" , current_video_rentals = None):
+        super().__init__(id, first_name, last_name, account_type, current_video_rentals)
+    
 
 
-class Customer_px:
-    pass
+class Customer_px(Customer):
+    
+    def __init__(self, id, first_name, last_name, account_type = "px" , current_video_rentals = None):
+        super().__init__(id, first_name, last_name, account_type, current_video_rentals)
 
 
-class Customer_sf:
-    pass
+class Customer_sf(Customer):
+    
+    def __init__(self, id, first_name, last_name, account_type = "sf" , current_video_rentals = None):
+        super().__init__(id, first_name, last_name, account_type, current_video_rentals)
 
 
-class Customer_pf:
-    pass
+class Customer_pf(Customer):
+    
+    def __init__(self, id, first_name, last_name, account_type = "pf" , current_video_rentals = None):
+        super().__init__(id, first_name, last_name, account_type, current_video_rentals)
