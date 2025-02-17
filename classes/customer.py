6034473d536bff555fa314b90faa0dd94783d196
jack-himlo <devon.g.jackson417@gmail.com>
2025-02-17 12:06:04 -0500

    Updated WIP, updated videos.py. immporting into customer.py

--- classes/customer.py
+++ classes/customer.py
@@ -1,4 +1,6 @@
 # Write you Customer Class here
+from classes.video import Video
+
 class Customer:
     customer = {}
     counter = 1
@@ -90,46 +92,86 @@ class Customer:
     def  create_a_customer_dict():
         valid_account_types = {"sx", "px", "sf", "pf"}
         # Customer.counter += 1
+        #require first name to be letters and longer than a single character 
         while True:
             first_name = input("Enter customer first name: ")
             if first_name.isalpha() and len(first_name) > 1:
                 break
             print("enter a valid first name")
+        #require last name to be letters and longer than a single character 
         while True:
             last_name = input("Enter customer last name: ")
             if last_name.isalpha() and len(last_name) > 1:
                 break
             print("enter a valid last name")
+        # require account type to valid input
         while True:
             account_type = input("Enter customer account type: ")
             if account_type.lower() in valid_account_types:
                 break
             print("Invalid account type. Please enter one of: sx, px, sf, pf.")
+        #returns customer data that was just added
         return {
             "id" : Customer.counter,
             "first_name" : first_name,
             "last_name" : last_name,
             "account_type" : account_type
         }
+    #define class level method to find customer by ID
     @classmethod
     def get_customer_by_id(cls):
+        #iterate through customer id list 
         while True:
             try:
                 customer_id = input("enter customer id to search: ")
                 customer_id = int(customer_id)
                 break
+            #require user to enter a number to search 
             except ValueError:
                 print("Invalid ID. PLease enter a number.")
+        #return customer if id is found
         if customer_id in cls.customer:
             return cls.customer[customer_id]
+        #inform user customer id didnt find customer
         else:
             return f"No customer was found with that id"
 
-
+    #define get customer rented videos method
     def get_customer_rented_videos(self):
+        #check if customer currently has any rentals
         if self._current_video_rentals:
             return f"{self.first_name} has the following rentals:\n{self._current_video_rentals}"
+        #if no rentals are found
         return f"{self.first_name} has no current rentals."
+    
+    #define rent a video method
+    def rent_a_video(self, video_title):
+        # from classes.video import Video
+        #check account type, sort by max number of rentals
+        if self._account_type.lower() == "sx" or self._account_type.lower() == "sf":
+            max_rentals = 1
+            #check i
+            if len(self._current_video_rentals) >= max_rentals:
+                return f"{self.first_name} has max amount of rentals"
+        elif self._account_type.lower() == "px" or self._account_type.lower() == "pf":
+            max_rentals = 3
+            if len(self._current_video_rentals) >= max_rentals:
+                return f"{self.first_name} has max amount of rentals"
+            
+        
+            
+
+    #define return a video
+    def return_a_video(self, video_title):
+        #check if title of movie is in customer rentals
+        if video_title not in self._current_video_rentals:
+            raise ValueError(f"{video_title} is not in your rentals.")
+        ##remove title from customer rentals to return
+        else:
+            self._current_video_rentals.remove(video_title)
+            return f"{self.first_name} has returned '{video_title}'."
+    
+
         
 
 
--- classes/video.py
+++ classes/video.py
@@ -1,3 +1,59 @@
 # Write your video Class here
+import csv
+
 class Video:
-    pass
+    videos = {}
+    
+
+    
+    def __init__(self, id, title, rating, release_year, copies_available):
+        self._id = int(id)
+        self._title = title
+        self._rating = rating
+        self._release_year = int(release_year)
+        self._copies_available = int(copies_available)
+    
+    #set attributes
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @property
+    def release_year(self):
+        return self._release_year
+
+    @property
+    def copies_available(self):
+        return self._copies_available
+    #set setter
+    @copies_available.setter
+    def copies_available(self,value):
+        #ensure value is not negative
+        if value < 0:
+            raise ValueError ("Copiesavailable cannot be negative ")
+        self._copies_available = value
+    @classmethod 
+    def load_data(cls, filename = "data/videos.csv"):
+        try:
+            with open("data/videos.csv") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    video = Video(
+                            id = row["id"],
+                            title = row["title"],
+                            rating  = row["rating"],
+                            release_year = int(row["release_year"]),
+                            copies_available = int(row["copies_available"]),
+                            )
+                    cls.videos[video.title] = video
+            print("Video data loaded successfully")
+        except FileNotFoundError:
+            print(f"Error: {filename} not found")
